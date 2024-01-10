a2364f686404e584afdfd26ed986d8212d5a998f
Richaljuliansyah <73353762+Richaljuliansyah@users.noreply.github.com>
2024-01-10 10:55:56 +0700

    Update bot_gula.py

--- bot_gula.py
+++ bot_gula.py
@@ -3,6 +3,7 @@ import os
 import mysql.connector
 import logging
 import matplotlib.pyplot as plt
+import json
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -17,11 +18,11 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="mydatabase"
+    database="mydatabase3"
 )
 
 # Inisialisasi bot
-bot = Bot(token="6174942077:AAGJ-8GzxIa_OfwNIF9owPTZGcTbgW-H9dY")
+bot = Bot(token="6760519530:AAHJPOBpwKzLidnVnehnrUSM3QFNAyYzik4")
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 # State untuk pengisian/edit profil
@@ -98,6 +99,12 @@ def profile_keyboard():
     keyboard.add(types.InlineKeyboardButton(text="Kembali", callback_data="back_to_menu"))
     return keyboard
 
+# Command /backtomenu
+@dp.callback_query_handler(text='back_to_menu')
+async def back_to_menu(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, "Kembali ke menu utama", reply_markup=menu_keyboard())
+
+
 # Command /editprofile
 @dp.callback_query_handler(text='edit_profile')
 async def edit_profile(callback_query: types.CallbackQuery):
@@ -173,22 +180,21 @@ async def process_edit_alamat(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['alamat'] = message.text
 
-    # Simpan data profil ke database
-    save_profile_data(data)
+    # Update data profil ke database
+    update_profile_data(message.from_user.id, data)
 
-    await message.reply("Profil berhasil disimpan.")
+    await message.reply("Profil berhasil diperbarui.")
 
     # Kembali ke menu profil
     await bot.send_message(message.chat.id, "Menu Profil", reply_markup=profile_keyboard())
-
-# Fungsi untuk menyimpan data profil ke database
-def save_profile_data(data):
+# Fungsi untuk menyimpan atau memperbarui data profil ke database
+def update_profile_data(user_id, data):
     mycursor = mydb.cursor()
-    sql = "INSERT INTO profile (nama, usia, jenis_kelamin, email, nomor_telepon, berat_badan, tinggi_badan, alamat) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (data['nama'], data['usia'], data['jenis_kelamin'], data['email'], data['nomor_telepon'], data['berat_badan'], data['tinggi_badan'], data['alamat'])
+    sql = "UPDATE pasien SET nama=%s, usia=%s, jenis_kelamin=%s, email=%s, nomor_telepon=%s, berat_badan=%s, tinggi_badan=%s, alamat=%s WHERE user_id=%s"
+    val = (data['nama'], data['usia'], data['jenis_kelamin'], data['email'], data['nomor_telepon'], data['berat_badan'], data['tinggi_badan'], data['alamat'], user_id)
     mycursor.execute(sql, val)
     mydb.commit()
-
+    
 # Command /viewprofile
 @dp.callback_query_handler(text='view_profile')
 async def view_profile(callback_query: types.CallbackQuery):
@@ -213,7 +219,7 @@ async def view_profile(callback_query: types.CallbackQuery):
 # Fungsi untuk mendapatkan data profil dari database
 def get_profile_data(user_id):
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM profile WHERE user_id = %s"
+    sql = "SELECT * FROM pasien WHERE user_id = %s"
     val = (user_id,)
     mycursor.execute(sql, val)
     result = mycursor.fetchone()
@@ -231,6 +237,7 @@ def get_profile_data(user_id):
         return profile_data
     else:
         return None
+
 # States untuk rekam medis baru
 class RekamMedisStates(StatesGroup):
     GULA_DARAH_SEWAKTU = State()
@@ -302,7 +309,6 @@ async def process_new_tekanan_darah_diastolik(message: types.Message, state: FSM
     await message.reply("Silakan masukkan catatan:")
     await RekamMedisStates.CATATAN.set()
 
-
 # Menangani input catatan saat rekam medis baru
 @dp.message_handler(state=RekamMedisStates.CATATAN)
 async def process_new_catatan(message: types.Message, state: FSMContext):
@@ -310,7 +316,7 @@ async def process_new_catatan(message: types.Message, state: FSMContext):
         data['catatan'] = message.text
 
     # Simpan data rekam medis ke database
-    save_rekam_medis_data(types.callback_query.from_user.id, data)
+    save_rekam_medis_data(message.from_user.id, data)
 
     await message.reply("Rekam medis berhasil disimpan.")
 
@@ -318,10 +324,11 @@ async def process_new_catatan(message: types.Message, state: FSMContext):
     await bot.send_message(message.chat.id, "Menu Rekam Medis", reply_markup=rekam_medis_keyboard())
 
 
+
 # Fungsi untuk menyimpan data rekam medis ke database
 def save_rekam_medis_data(user_id, data):
     mycursor = mydb.cursor()
-    sql = "INSERT INTO rekam_medis (user_id, gula_darah_sewaktu, gula_darah_puasa, tekanan_darah_sistolik, tekanan_darah_diastolik, catatan) VALUES (%s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO report (user_id, gula_darah_sewaktu, gula_darah_puasa, tekanan_darah_sistolik, tekanan_darah_diastolik, catatan) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (user_id, data['gula_darah_sewaktu'], data['gula_darah_puasa'], data['tekanan_darah_sistolik'], data['tekanan_darah_diastolik'], data['catatan'])
     mycursor.execute(sql, val)
     mydb.commit()
@@ -345,49 +352,27 @@ async def view_rekam_medis(callback_query: types.CallbackQuery):
     else:
         await bot.send_message(callback_query.from_user.id, "Tidak ada data rekam medis.")
 
-
 # Fungsi untuk mengambil data rekam medis dari database
 def get_rekam_medis_data(user_id):
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM rekam_medis WHERE user_id = %s"
+    sql = "SELECT * FROM report WHERE user_id = %s"
     val = (user_id,)
     mycursor.execute(sql, val)
-    return mycursor.fetchall()
+    result = mycursor.fetchall()
+    mycursor.close()
 
-
-# Command /deleterekammedis
-@dp.callback_query_handler(text='delete_rekam_medis')
-async def delete_rekam_medis(callback_query: types.CallbackQuery):
-    await bot.send_message(callback_query.from_user.id, "Masukkan ID rekam medis yang ingin dihapus:")
-    await RekamMedisStates.DELETE_ID.set()
-
-
-# Menangani input ID rekam medis yang ingin dihapus
-@dp.message_handler(state=RekamMedisStates.DELETE_ID)
-async def process_delete_rekam_medis(message: types.Message, state: FSMContext):
-    delete_id = message.text
-    delete_success = delete_rekam_medis_data(types.callback_query.from_user.id, delete_id)
-
-    if delete_success:
-        await message.reply("Rekam medis berhasil dihapus.")
-    else:
-        await message.reply("Gagal menghapus rekam medis. Pastikan ID yang Anda masukkan benar.")
-
-    # Kembali ke menu rekam medis
-    await bot.send_message(message.chat.id, "Menu Rekam Medis", reply_markup=rekam_medis_keyboard())
+    return result
 
 
 # Fungsi untuk menghapus data rekam medis dari database
 def delete_rekam_medis_data(user_id, delete_id):
     mycursor = mydb.cursor()
-    sql = "DELETE FROM rekam_medis WHERE user_id = %s AND id = %s"
+    sql = "DELETE FROM report WHERE user_id = %s AND id = %s"
     val = (user_id, delete_id)
     mycursor.execute(sql, val)
     mydb.commit()
     return mycursor.rowcount > 0
 
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Command /insight
 @dp.callback_query_handler(text='insight')
@@ -395,69 +380,73 @@ async def insight(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
 
     mycursor = mydb.cursor()
-    sql = "SELECT gula_darah_sewaktu, gula_darah_puasa, tekanan_darah_diastolik, tekanan_darah_sistolik FROM rekam_medis WHERE user_id = %s"
+    sql = "SELECT gula_darah_sewaktu, gula_darah_puasa, tekanan_darah_diastolik, tekanan_darah_sistolik FROM report WHERE user_id = %s"
     val = (user_id,)
     mycursor.execute(sql, val)
     results = mycursor.fetchall()
     mycursor.close()
 
     if results:
-        gula_darah_sewaktu = []
-        gula_darah_puasa = []
-        tekanan_darah_diastolik = []
-        tekanan_darah_sistolik = []
+        # Memproses data dan membuat grafik
+        create_chart('Gula Darah Sewaktu', results, 140, 'gula_darah_sewaktu_chart.png')
+        create_chart('Gula Darah Puasa', results, 140, 'gula_darah_puasa_chart.png')
+        create_chart('Tekanan Darah Diastolik', results, 100, 'tekanan_darah_diastolik_chart.png')
+        create_chart('Tekanan Darah Sistolik', results, 140, 'tekanan_darah_sistolik_chart.png')
 
-        for row in results:
-            gula_darah_sewaktu.append(float(row[0]))
-            gula_darah_puasa.append(float(row[1]))
-            tekanan_darah_diastolik.append(float(row[2]))
-            tekanan_darah_sistolik.append(float(row[3]))
-
-        labels = ['Gula Darah Sewaktu', 'Gula Darah Puasa', 'Tekanan Darah Diastolik', 'Tekanan Darah Sistolik']
-        values = [
-            len(gula_darah_sewaktu),
-            len(gula_darah_puasa),
-            len(tekanan_darah_diastolik),
-            len(tekanan_darah_sistolik)
-        ]
-        title = 'Presentase Data Medis'
-        normal_limit_diastolik = 80
-        normal_limit_sistolik = 120
-
-        # Hitung total nilai
-        total = sum(values)
-
-        # Hitung presentase setiap nilai
-        percentages = [(value / total) * 100 for value in values]
-
-        # Buat diagram
-        fig, ax = plt.subplots()
-        ax.pie(percentages, labels=labels, autopct='%1.1f%%', startangle=90)
-
-        # Tambahkan batas normal tekanan darah diastolik
-        ax.axhline(y=normal_limit_diastolik, color='r', linestyle='--', label='Batas Normal Diastolik')
-
-        # Tambahkan batas normal tekanan darah sistolik
-        ax.axhline(y=normal_limit_sistolik, color='g', linestyle='--', label='Batas Normal Sistolik')
-
-        ax.set_title(title)
-        ax.axis('equal')  # Memastikan diagram berbentuk lingkaran
-        ax.legend()
-
-        # Simpan diagram ke file
-        chart_filename = 'percentage_chart.png'
-        plt.savefig(chart_filename)
-        plt.close()  # Menutup gambar yang sedang digunakan
-
-        # Mengirim diagram presentase ke user
-        with open(chart_filename, 'rb') as photo:
+        # Mengirim grafik ke pengguna
+        with open('gula_darah_sewaktu_chart.png', 'rb') as photo:
+            await bot.send_photo(callback_query.from_user.id, photo)
+        with open('gula_darah_puasa_chart.png', 'rb') as photo:
+            await bot.send_photo(callback_query.from_user.id, photo)
+        with open('tekanan_darah_diastolik_chart.png', 'rb') as photo:
+            await bot.send_photo(callback_query.from_user.id, photo)
+        with open('tekanan_darah_sistolik_chart.png', 'rb') as photo:
             await bot.send_photo(callback_query.from_user.id, photo)
 
-        os.remove(chart_filename)
+        # Menghapus file gambar setelah dikirim
+        os.remove('gula_darah_sewaktu_chart.png')
+        os.remove('gula_darah_puasa_chart.png')
+        os.remove('tekanan_darah_diastolik_chart.png')
+        os.remove('tekanan_darah_sistolik_chart.png')
 
         await bot.send_message(callback_query.from_user.id, "Menu Utama", reply_markup=menu_keyboard())
     else:
         await bot.send_message(callback_query.from_user.id, "Data rekam medis tidak ditemukan!")
+
+# Fungsi untuk membuat grafik
+def create_chart(parameter, data, normal_limit, filename):
+    data_column_index = 0  # Ganti dengan indeks kolom yang sesuai dengan parameter
+    values = [float(row[data_column_index]) for row in data]
+    labels = [parameter, 'Others']
+    percentages = [(value / sum(values)) * 100 for value in values]
+
+    # Buat diagram
+    fig, ax = plt.subplots()
+    wedges, texts, autotexts = ax.pie(
+        percentages,
+        labels=labels,
+        autopct='%1.1f%%',
+        startangle=90,
+        colors=['skyblue', 'lightgrey'],
+        wedgeprops=dict(width=0.4),
+    )
+
+    # Tambahkan angka pada setiap sektor
+    for autotext in autotexts:
+        autotext.set_color('white')
+        autotext.set_size(8)  # Atur ukuran huruf agar muat
+
+    # Tambahkan batas normal
+    ax.axhline(y=normal_limit, color='r', linestyle='--', label=f'Batas Normal {parameter}')
+
+    ax.set_title(f'{parameter} Overview')
+    ax.axis('equal')
+    ax.legend()
+
+    # Simpan diagram ke file
+    plt.savefig(filename)
+    plt.close()
+
 
 # Command /backtomenu
 @dp.callback_query_handler(text='back_to_menu')
@@ -465,6 +454,6 @@ async def back_to_menu(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, "Kembali ke menu utama", reply_markup=menu_keyboard)
 
 # Memulai bot
-print("program system berhasil dijalankan yuhuuuuuuuuuuu")
+print("program system anda berhasil dijalankan")
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
